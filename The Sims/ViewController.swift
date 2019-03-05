e3b58d7b3300f3222a4c345287d07d971ae46703
Aleksander Bernat <wujekfester@aleksanders-macbook.local>
2019-03-05 20:36:56 +0100

    Initial Commit

---
+++ The Sims/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  The Sims
+//
+//  Created by Aleksander Bernat on 05/03/2019.
+//  Copyright © 2019 Aleksander Bernat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
